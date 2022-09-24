a3075461c11b37831ad7d450d746d2b53b9f89fd
CrocodileGenna <yuramus1998@gmail.com>
2022-09-24 16:15:59 +0300

    comit 4 fix bugs

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -1,14 +1,3 @@
-// export async function fetchSearch(serch){
-//     // const axios = require('axios');
-//      let nomber = 1;
-//      const axios = await fetch(`https://pixabay.com/api/?key=30100311-f3864219c2c65e8e904a2d1d0&q=${serch}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${nomber}`)
-//      const axiosIn = await axios.json()
-//      console.log(axiosIn)
-//      return axiosIn
-//      // return axios.get(`https://pixabay.com/api/?key=30100311-f3864219c2c65e8e904a2d1d0&q=${serch}&image_type=photo&orientation=horizontal&safesearch=true`)
-//     // .then()
-// }
-
 import axios from "axios";
 const BASE_URL = "https://pixabay.com/api/";
 
